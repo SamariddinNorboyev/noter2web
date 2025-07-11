f8190248e6132aa4cbbc1e835d513c2521613a90
“Norboyev <samariddin.grex@gmail.com>
2025-07-11 11:30:18 +0500

    a

--- noter2web/urls.py
+++ noter2web/urls.py
@@ -20,9 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-from django.views.static import serve
-import os
 
 
 urlpatterns = [
